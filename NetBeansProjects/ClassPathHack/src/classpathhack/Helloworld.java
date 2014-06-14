73dfe1d2e54cbe7cdab448e8df325879e876050f
daisyab <nkweteyimdaisy@gmail.com>
2014-06-14 15:32:51 +0100

    Create Helloworld.java

---
+++ NetBeansProjects/ClassPathHack/src/classpathhack/Helloworld.java
@@ -0,0 +1,11 @@
+package classpathhack;
+
+/**
+ *
+ * @author guestu
+ */
+public class Helloworld extends GeneratedClass{
+    public static void main(String[] args){
+        System.out.println("Hello everyone");
+    }
+}
